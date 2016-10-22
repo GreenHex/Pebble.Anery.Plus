dbd8c80beead305a728245565f3e66fdd55f9f38
GreenHex <GreenHex@gmail.com>
2016-10-22 22:42:36 +0530

    Initial commit
    Pulled BlissTime2 and compiled.

--- src/c/main.c
+++ src/c/main.c
@@ -1,12 +1,19 @@
 #include <pebble.h>
 #include "global.h"
+#include "calendar.h"
 #include "clock.h"
+#include "status_disp.h"
+#include "battery.h"
 #include "config.h"
+#include "chime.h"
 #include "app_messaging.h"
 
 static Window *window;
 
 static void window_load( Window *window ) { // order is important as layers are drawn over each other.
+  calendar_init( window );
+  status_init( window );
+  battery_init( window );
   clock_init( window );
   messaging_init();
 }
@@ -14,6 +21,9 @@ static void window_load( Window *window ) { // order is important as layers are 
 static void window_unload( Window *window ) {
   messaging_deinit();
   clock_deinit();
+  battery_deinit();
+  status_deinit();
+  calendar_deinit();
 }
 
 static void init( void ) {
